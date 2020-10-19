7cd60ea12c86adbf3bfdc94bca252a525d3c4e8e
Alex <ovcharenko@octarine.com.ua>
2020-10-19 23:07:15 +0300

    feat: add reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let number, result = 0;
+    if (n < 0) {
+        n=n*(-1);
+    }
+    while(n){
+        number = n % 10;               
+        result = (result * 10) + number;
+        n = n/10|0;                     
+    }  
+    return result;
 }
